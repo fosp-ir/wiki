6032af13e6395a7731ec50b29904706880ef0552
Ali Safamanesh <daradege@proton.me>
2025-10-19 13:27:37 +0330

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -13,20 +13,6 @@ extensions = [
     'sphinx_copybutton',
     'sphinx_design',
 ]
-# -- AutoAPI configuration -------------------------------------------------
-autoapi_dirs = ['../../pyrobale']
-autoapi_type = 'python'
-autoapi_root = 'api'
-autoapi_options = [
-    'members',
-    'undoc-members',
-    'show-inheritance',
-    'show-module-summary',
-    'special-members',
-]
-
-autoapi_add_toctree_entry = True
-autoapi_keep_files = False
 
 # -- Napoleon settings -----------------------------------------------------
 napoleon_google_docstring = True
@@ -36,26 +22,26 @@ napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
 
 # -- General configuration -------------------------------------------------
-project = 'Pyrobale'
-copyright = '2025, Ali Safamanesh & Aydin Rahbaran'
-author = 'Ali Safamanesh & Aydin Rahbaran'
+project = 'Fosp!'
+copyright = '2025, Ali Safamanesh'
+author = 'Ali Safamanesh'
 
 # -- HTML output -----------------------------------------------------------
 html_theme = 'furo'
 html_theme = 'furo'
-html_title = 'Pyrobale Documentation'
+html_title = 'Fosp Documentation'
 
 html_theme_options = {
     "top_of_page_button": "arrow",
-    "source_repository": "https://github.com/pyrobale/pyrobale",
+    "source_repository": "https://github.com/fosp-ir/fosp-ir",
     "source_branch": "main",
     "source_directory": "docs/source/",
     "navigation_with_keys": True,
-    "announcement": "<strong>🚀 Pyrobale Beta!</strong> - Modern library for Bale bot API",
+    "announcement": "<strong>🚀 Fosp Beta!</strong>",
     "footer_icons": [
         {
             "name": "GitHub",
-            "url": "https://github.com/yourusername/pyrobale",
+            "url": "https://github.com/fosp-ir/fosp-ir",
             "html": """
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
